9253e30536b39dbf85eccf4355a3773975cd086a
Philippe Recto <precto1285@gmal.com>
2019-04-22 20:51:50 -0700

    updated files with images and featured app

---
+++ src/components/featured/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Featured = () => {
+  return (
+    <div style={{ position: 'relative' }}>
+      <div className="artist_name">
+        <div className="wrapper">
+          Ariana Grande
+    </div></div>
+    </div>
+  );
+};
+
+export default Featured;
